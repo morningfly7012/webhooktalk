99b503592f0358bc28c9ccc1af12325fdd95820a
moningfly <79407773+morningfly7012@users.noreply.github.com>
2022-06-29 19:59:16 +0800

    Update webhooktalk.py

--- webhooktalk.py
+++ webhooktalk.py
@@ -1,11 +1,3 @@
-'''
-Author: Travis CI travis@travis-ci.org
-Date: 2022-06-06 10:04:41
-LastEditors: Travis CI travis@travis-ci.org
-LastEditTime: 2022-06-06 14:25:13
-FilePath: \c:Users\morni\OneDrive - 晨飛\桌面\test.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 from discord import SyncWebhook
 
 webhooklink = input('請輸入你的webhooks：')
